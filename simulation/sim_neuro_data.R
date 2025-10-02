45d20ffc84af2a712a1540af2df8125f698fbea3
Spencer Wadsworth <wadsworth.spencer.g@gmail.com>
2025-10-02 17:59:41 -0400

    more variable simulation

--- simulation/sim_neuro_data.R
+++ simulation/sim_neuro_data.R
@@ -36,46 +36,7 @@ dur <- list(0, 0, 0, 0)
 
 
 
-region.1A.center <- c(13,13)
-region.1A.radius <- 4
-region.1B.center <- c(40,18)
-region.1B.radius <- 6
-# region.1C.center <- c(10,45,24)
-# region.1C.radius <- 3
-# region.2.center <- c(15,16,31)
-# region.2.radius <- 5
-# region.3.center <- c(12,16,13)
-# region.3.radius <- 5
-coord.regions <- list(region.1A.center, region.1B.center)
-radius.regions <- c(region.1A.radius,region.1B.radius)
-onsets.regions <- list(onsets, onsets)
-dur.regions <- list(dur, dur)
 
-
-# region.1a.d <- list(160.46, 140.19, 200.16, 160.69)
-# region.1b.d <- list(140.51, 120.71, 160.55, 120.44)
-# region.1c.d <- list(120.53, 120.74, 140.02, 100.48)
-# region.2.d <- list( -0.24, 10.29, 80.18, 160.24)
-# region.3.d <- list(192.7, 50.04, 240.60, 50.83)
-
-region.1a.d <- as.list(rnorm(4, .75, .5))
-region.1b.d <- as.list(rnorm(4, .6, .5))
-# region.1c.d <- as.list(rnorm(4, 1.5, .5))
-# region.2.d <- as.list(rnorm(4, 1.5, .5))
-# region.3.d <- as.list(rnorm(4, 1.5, .5))
-effect <- list(region.1a.d,region.1b.d)
-
-
-
-
-
-design <- simprepTemporal(regions=2,
-                          onsets=onsets.regions, durations=dur.regions,
-                          hrf="double-gamma", TR=TR, totaltime=total.time,
-                          effectsize=effect)
-spatial <- simprepSpatial(regions=2,
-                          coord=coord.regions, radius=radius.regions,
-                          form="sphere", fading=0.05)
 
 
 
@@ -84,6 +45,48 @@ sub_data <- list()
 
 hdr_sim <- data.frame()
 for (i in 1:length(subjects)) {
+  
+  
+  region.1A.center <- c(13,13)
+  region.1A.radius <- ceiling(rnorm(1, 3.5, .56))
+  region.1B.center <- c(40,18)
+  region.1B.radius <- ceiling(rnorm(1, 5.5, .5))
+  # region.1C.center <- c(10,45,24)
+  # region.1C.radius <- 3
+  # region.2.center <- c(15,16,31)
+  # region.2.radius <- 5
+  # region.3.center <- c(12,16,13)
+  # region.3.radius <- 5
+  coord.regions <- list(region.1A.center, region.1B.center)
+  radius.regions <- c(region.1A.radius,region.1B.radius)
+  onsets.regions <- list(onsets, onsets)
+  dur.regions <- list(dur, dur)
+  
+  
+  # region.1a.d <- list(160.46, 140.19, 200.16, 160.69)
+  # region.1b.d <- list(140.51, 120.71, 160.55, 120.44)
+  # region.1c.d <- list(120.53, 120.74, 140.02, 100.48)
+  # region.2.d <- list( -0.24, 10.29, 80.18, 160.24)
+  # region.3.d <- list(192.7, 50.04, 240.60, 50.83)
+  
+  region.1a.d <- as.list(rnorm(4, .75, .5))
+  region.1b.d <- as.list(rnorm(4, .6, .5))
+  # region.1c.d <- as.list(rnorm(4, 1.5, .5))
+  # region.2.d <- as.list(rnorm(4, 1.5, .5))
+  # region.3.d <- as.list(rnorm(4, 1.5, .5))
+  effect <- list(region.1a.d,region.1b.d)
+  
+  
+  
+  
+  
+  design <- simprepTemporal(regions=2,
+                            onsets=onsets.regions, durations=dur.regions,
+                            hrf="double-gamma", TR=TR, totaltime=total.time,
+                            effectsize=effect)
+  spatial <- simprepSpatial(regions=2,
+                            coord=coord.regions, radius=radius.regions,
+                            form="sphere", fading=0.05)
   sub_data[[i]] <- simVOLfmri(design=design, image=spatial,
                               base=0, SNR=.3, noise="spatial", type="rician",
                               rho.temp=c(0.142,0.108,0.084), rho.spat=0.4,
