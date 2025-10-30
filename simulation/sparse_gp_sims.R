e873b0beb19097e7334e23f596c81eb57ec105b9
Spencer Wadsworth <wadsworth.spencer.g@gmail.com>
2025-10-30 09:03:41 -0400

    finish sparse gp sim

--- simulation/sparse_gp_sims.R
+++ simulation/sparse_gp_sims.R
@@ -1,8 +1,8 @@
 source("./simulation_functions.R")
 
 
-nsubjects <- 10
-pmethod <- "shift"
+nsubjects <- 5
+pmethod <- "sgp"
 K <- 40
 M <- 500
 set.seed(21)
@@ -11,16 +11,16 @@ seeds <- sample(2^31-1, M, replace = FALSE)
 mod <- cmdstan_model("../stan_models/spat_nngp_lm_hs_sb_ar1.stan", 
                      include_paths = gptools_include_path())
 
-
+m <- 4
 plan(multisession, workers = min(M, 110))  # Windows-friendly
 res <- future_lapply(1:M,  
           function(m) {
             
-            testvox <- expand.grid(x = 9:12, y = 7:9) %>% 
-              mutate(voxel = paste(x, y, sep = "_"))
+            # testvox <- expand.grid(x = 26:29, y = 15:17) %>% 
+            #   mutate(voxel = paste(x, y, sep = "_"))
             sims_test <- sim_fmri_data(nsubs = nsubjects, seed = seeds[m])
             hdr_df <- sims_test[[2]] %>% 
-              filter(time <= 150, voxel %in% testvox$voxel)
+              filter(time <= 150)#, voxel %in% testvox$voxel)
             
              
             stan_data <- get_stan_data(hdr_df)
@@ -43,8 +43,10 @@ res <- future_lapply(1:M,
             all_res <- get_gp_act_res(hdr_df, param_sums) %>% 
               mutate(m = m, method = "sgp")
             
+            all_res
+            
           
         })
 
 
-
+saveRDS(res, paste0("./sim_res/sgp_", pmethod, "_", nsubjects, ".rds"))
