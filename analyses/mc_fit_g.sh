69e50767e739d52a88aa45b43ebf80da649510ea
iac25002 <iac25002@login5.storrs.hpc.uconn.edu>
2025-12-08 15:31:13 -0500

    loop changes

--- analyses/mc_fit_g.sh
+++ analyses/mc_fit_g.sh
@@ -12,6 +12,6 @@
 module purge
 module load gsl/2.8 udunits/2.2.28-gcc14.2 cuda/11.6 freetype/2.12.1 gdal/3.9.2 r/4.4.2 proj geos cmake
 #
-Rscript ./clean_analysis_3D.R "$1" "$2" "$3" "$4" "$5"
+Rscript ./clean_analysis_3D.R "$1" "$2" "$3" "$4" "$5" "$6"
 #Rscript ./cyc_shff_test.R
 #Rscript ./four_scamble_test.R
